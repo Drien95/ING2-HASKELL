a6e227eaf073480e41b22d2a18ccda9d7cf60f16
Romcast <129684627+Romcast@users.noreply.github.com>
2024-03-19 15:20:06 +0100

    Update Source.hs

--- src/Statistic/Source.hs
+++ src/Statistic/Source.hs
@@ -6,17 +6,20 @@
 
 module Statistic.Source(occurrences, entropy, orderedCounts) where
 
-import Data.Map (Map)
+import Data.Map (Map, insertWith, elems, toList, empty)
+import Data.List (sortOn)
 
 -- | The map giving occurrences of each symbol in the source
 occurrences :: Ord a => [a] -> Map a Int
-occurrences _ = undefined -- TODO
+occurrences source = foldl (\m x -> insertWith (+) x 1 m) empty source 
 
 -- | SHANNON entropy of source
 entropy :: Ord a => [a] -> Double
-entropy _ = undefined -- TODO
+entropy source = -(foldl (\acc x -> ((fromIntegral x)/l) * logBase 2 ((fromIntegral x)/l) + acc) 0.0 (elems (occurrences source)))
+  where l = fromIntegral (length source)
+
 
 -- | List of occurrences ordered by count
 orderedCounts :: Ord a => [a] -> [(a, Int)]
-orderedCounts _ = undefined -- TODO
+orderedCounts source = sortOn (\tuple -> snd tuple) (toList (occurrences source))
 
