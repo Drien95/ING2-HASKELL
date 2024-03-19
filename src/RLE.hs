ab163022ac80f383c6cd9dec3f2a5e8e726b6420
Anatpqs <120056597+Anatpqs@users.noreply.github.com>
2024-03-19 16:02:34 +0100

    Update RLE.hs

--- src/RLE.hs
+++ src/RLE.hs
@@ -3,13 +3,21 @@
   Description : An implementation of the run-length encoding method
   Maintainer  : ???
 -}
-module RLE(compress, uncompress) where
+module RLE (compress, uncompress) where
 
 -- | RLE compress method
 compress :: Eq a => [a] -> [(a, Int)]
-compress _ = undefined -- TODO
+compress [] = []
+compress (x:xs) = let (packed, rest) = span (== x) xs
+                      count = 1 + length packed
+                  in (x, count) : compress rest
 
 -- | RLE uncompress method
 -- If input cannot be uncompressed, returns `Nothing`
 uncompress :: [(a, Int)] -> Maybe [a]
-uncompress _ = undefined -- TODO
+uncompress [] = Just []
+uncompress ((x, count):xs)
+  | count <= 0 = Nothing
+  | otherwise = case uncompress xs of
+                  Nothing -> Nothing
+                  Just rest -> Just (replicate count x ++ rest)
